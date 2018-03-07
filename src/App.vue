78f225e90986abb3255ee7703967127e676285e1
re-z <re-z@ukr.net>
2018-03-07 21:33:32 +0200

    result component done

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <template>
   <div id="app">
-    <start v-if="state == 'start'" @started="changeStateToQuestion"></start>
-    <question
-      v-else-if="state == 'question'"
-      @correctAnswer="handleCorrectAnswer"
-      @wrongAnswer="handleWrongAnswer"
-    ></question>
-    <message v-else-if="state == 'message'"
-      :title="this.message.title"
-      :className = "this.message.className"
-      :correctAnswer = "this.message.correctAnswer"
-      @messageReaded = "changeStatetoResult"
-    ></message>
-    <result v-else-if="state == 'result'"></result>
+    <h1>Math trainer</h1>
+    <hr>
+    <div class="progress">
+      <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+    </div>
+    <div id="box">
+      <transition name="flip" mode="out-in">
+        <start v-if="state == 'start'" @started="changeStateToQuestion"></start>
+        <question
+          v-else-if="state == 'question'"
+          @correctAnswer="handleCorrectAnswer"
+          @wrongAnswer="handleWrongAnswer"
+        ></question>
+        <message v-else-if="state == 'message'"
+          :title="this.message.title"
+          :className = "this.message.className"
+          :correctAnswer = "this.message.correctAnswer"
+          @messageReaded = "changeStatetoResult"
+        ></message>
+        <result v-else-if="state == 'result'"></result>
+      </transition>
+    </div>
+
   </div>
 
 </template>
@@ -26,7 +36,17 @@ export default {
         title: '',
         className: '',
         correctAnswer: ''
-      }
+      },
+      stats: {
+        success: 0,
+        errors: 0,
+      },
+      maxQuestions: 3
+    }
+  },
+  computed: {
+    questionsDone() {
+      return this.stats.success + this.stats.errors
     }
   },
   methods: {
@@ -34,7 +54,8 @@ export default {
       this.state = 'question'
     },
     changeStatetoResult () {
-      this.state = 'result'
+      // this.state = 'result'
+      this.state = 'question'
     },
     handleCorrectAnswer() {
       this.state = "message";
@@ -44,7 +65,7 @@ export default {
     handleWrongAnswer(msg){
       this.state = "message";
       this.message.title = 'Thats wrong answer!';
-      this.message.className = 'alert-warning'
+      this.message.className = 'alert-warning';
       this.message.correctAnswer = 'The correct answer is '+ msg
     }
   }
@@ -56,4 +77,22 @@ export default {
     max-width: 900px
     margin: 20px auto
     text-align: center
+  .flip-enter
+  .flip-enter-active
+    animation: flipInX .3s linear
+  .flip-leave
+  .flip-leave-active
+    animation: flipOutX .3s linear
+  @keyframes flipInX
+    from
+      transform: rotateX(90deg)
+    to
+      transform: rotateX(0deg)
+  @keyframes flipOutX
+    from
+      transform: rotateX(0deg)
+    to
+      transform: rotateX(90deg)
+
+
 </style>
--- src/components/start.vue
+++ src/components/start.vue
@@ -1,6 +1,6 @@
 <template>
   <div  class="wrap alert alert-secondary">
-    <h1>Are you ready to test your Math skills?</h1>
+    <h2>Are you ready to test your Math skills?</h2>
     <button class="start__btn btn btn-success" @click="$emit('started')">Lets go</button>
   </div>
 </template>
